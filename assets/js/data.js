1bf65061058e88abba11044c313c72702829ab9a
Joice Martins <joicemartins.dev@gmail.com>
2023-05-07 21:10:26 -0300

    removido arquivo desnecessário

--- assets/js/data.js
+++
@@ -1,8 +0,0 @@
-const data = document.querySelector('#data-hoje p');
-
-mes = new Array ("janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro")
-const hoje = new Date();
-
-data.innerHTML = (mes[hoje.getMonth()] +" / "+ hoje.getFullYear());
-
-
